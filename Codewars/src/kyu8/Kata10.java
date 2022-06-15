2dfa76555251bf8f6e437f5acef727a7c8bdd2ae
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-06-15 19:54:53 +0530

    Commit Kata10 - array sequence reverse java code

---
+++ Codewars/src/kyu8/Kata10.java
@@ -0,0 +1,24 @@
+package kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+
+public class Kata10 {
+        @Test
+        public void simpleTest() {
+            assertArrayEquals(new int[]{5,4,3,2,1},Sequence.reverse(5));
+        }
+}
+
+class Sequence{
+    public static int[] reverse(int n) {
+        int[] array = new int[n];
+        for (int i = n; i > 0; i--) {
+            array[n-i] = i;
+        }
+        return array;
+    }
+
+}
+
