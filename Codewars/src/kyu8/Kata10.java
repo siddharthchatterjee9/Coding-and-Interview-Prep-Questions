13f97ff2211a514acc45edd71fb96b365df96cbc
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-06-16 00:31:01 +0530

    Commit Kata10 - reverse array java code from a particular number n

--- Codewars/src/kyu8/Kata10.java
+++ Codewars/src/kyu8/Kata10.java
@@ -2,13 +2,12 @@ package kyu8;
 
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
-import org.junit.runners.JUnit4;
 
 public class Kata10 {
-        @Test
-        public void simpleTest() {
-            assertArrayEquals(new int[]{5,4,3,2,1},Sequence.reverse(5));
-        }
+    @Test
+    public void simpleTest() {
+        assertArrayEquals(new int[]{5,4,3,2,1},Sequence.reverse(5));
+    }
 }
 
 class Sequence{
