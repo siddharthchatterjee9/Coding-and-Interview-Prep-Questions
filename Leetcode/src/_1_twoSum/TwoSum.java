35fb061113f1363493abb1b3f4243bc9dd58e90b
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-07-14 23:43:01 +0530

    Commit Two Sum java solution built using HashMap! (runtime is faster than normal double for loop method)

--- Leetcode/src/_1_twoSum/TwoSum.java
+++ Leetcode/src/_1_twoSum/TwoSum.java
@@ -4,7 +4,7 @@ package _1_twoSum;
 
 import java.util.HashMap;
 
-public class Solution {
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
