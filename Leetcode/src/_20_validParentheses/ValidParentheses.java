eb2ef281316a473f4f6512a862b99164a5e16f5e
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-08-07 01:59:04 +0530

    Commit ValidParentheses.java solution using Stack

---
+++ Leetcode/src/_20_validParentheses/ValidParentheses.java
@@ -0,0 +1,32 @@
+package _20_validParentheses;
+
+// https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }
+            else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
