908bea4bbf46c20aac347322e34ce28488a29561
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-08-16 16:11:50 +0530

    Commit RomanToInteger.java solution

---
+++ Leetcode/src/_13_romanToInteger/RomanToInteger.java
@@ -0,0 +1,38 @@
+package _13_romanToInteger;
+
+// https://leetcode.com/problems/roman-to-integer/
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'V' || s.charAt(i) == 'X') {
+                if (i > 0 && s.charAt(i-1) == 'I') {
+                    sum += -2;
+                }
+            }
+            if (s.charAt(i) == 'L' || s.charAt(i) == 'C') {
+                if (i > 0 && s.charAt(i-1) == 'X') {
+                    sum += -20;
+                }
+            }
+            if (s.charAt(i) == 'D' || s.charAt(i) == 'M') {
+                if (i > 0 && s.charAt(i-1) == 'C') {
+                    sum += -200;
+                }
+            }
+            sum += map.get(s.charAt(i));
+        }
+        return sum;
+    }
+}
