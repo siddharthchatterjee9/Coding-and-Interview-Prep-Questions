ae4c028d3bf58feb28fcafeb41cc038f575ce446
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-08-20 09:34:51 +0530

    Commit ImplementStr.java using indexOf method

---
+++ Leetcode/src/_28_implementStr/ImplementStr.java
@@ -0,0 +1,9 @@
+package _28_implementStr;
+
+// https://leetcode.com/problems/implement-strstr/
+
+public class ImplementStr {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
