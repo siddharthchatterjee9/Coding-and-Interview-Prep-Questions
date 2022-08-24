281049303c68a38151692496b306ced5c4b3f5c5
Siddharth Chatterjee <siddharthchatterjee99@gmail.com>
2022-08-24 18:44:55 +0530

    Commit PalindromeLinkedList.java solution

---
+++ Leetcode/src/_234_palindromeLinkedList/PalindromeLinkedList.java
@@ -0,0 +1,44 @@
+package _234_palindromeLinkedList;
+
+// https://leetcode.com/problems/palindrome-linked-list/
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = reverseLinkedList(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    public ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next_node = head.next;
+            head.next = prev;
+            prev = head;
+            head = next_node;
+        }
+        return prev;
+    }
+}
